e668db1aed00baad9a22fefb2fd15a0e47f8c1c0
EdisonLeung <edisonleung2003@outlook.com>
2019-02-16 15:14:53 -0800

    add comments to C_Climb

--- src/main/java/frc/robot/commands/C_Climb.java
+++ src/main/java/frc/robot/commands/C_Climb.java
@@ -11,8 +11,6 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class C_Climb extends Command {
-  private double frontAngleError = 0;
-  private double rearAngleError = 0;
   private double angleMultiplier = 10;
   private static final double FRONT_CLIMBER_RADIUS = 16;
   private static final double REAR_CLIMBER_RADIUS = Math.sqrt(Math.pow(27, 2) + Math.pow(4, 2));
@@ -33,8 +31,14 @@ public class C_Climb extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    /*
+    TESTING
+    1.) figure out which direction the robot is tilted for a (-) output value from gyro
+    2.) figure out if the balancing works without controller
+    3.) figure out if the balancing works with the controller
+    */
     double targetAngle =  Robot.getOI().getTestController().getLeftYValue() * angleMultiplier;
-    Robot.getFrontClimber().goToDegree((targetAngle + getAngleError()));//needs testing
+    Robot.getFrontClimber().goToDegree((targetAngle + getAngleError()));
     Robot.getRearClimber().goToDegree(-FRONT_REAR_RATIO * (targetAngle + getAngleError()));
   }
 
