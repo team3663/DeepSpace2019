43ccac7dc12419a645a5f55a73c1ccb20b6c150a
EricHoelscher <erichoelscher@icloud.com>
2019-02-22 19:57:22 -0800

    Bring C_ExtendHatch from master branch

---
+++ src/main/java/frc/robot/commands/C_ExtendHatch.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class C_ExtendHatch extends Command {
+
+  private boolean state;
+
+  public C_ExtendHatch(boolean state) {
+    requires(Robot.getHatch());
+    this.state = state;
+  }
+
+  @Override
+  protected void execute() {
+    Robot.getHatch().extendHatchPickup(state);
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return true;
+  }
+
+}
