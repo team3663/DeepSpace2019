fae8c4dd6f21579c91a953a54b161576d6ad6307
RandomPotato26 <random.potato26@gmail.com>
2019-03-15 19:19:46 -0700

    add smart hatch deploy

---
+++ src/main/java/frc/robot/commands/command_groups/CG_SmartHatchDeploy.java
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.command_groups;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.commands.C_GoToRelativeLevel;
+import frc.robot.commands.C_GoToSelectedLevel;
+import frc.robot.commands.C_SetHatchClosed;
+import frc.robot.commands.C_Wait;
+
+public class CG_SmartHatchDeploy extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public CG_SmartHatchDeploy() {
+    addSequential(new CG_SmartHatchDeploy());
+    addSequential(new C_SetHatchClosed(false));
+    addSequential(new C_GoToRelativeLevel(-1));
+    addSequential(new C_Wait(1000));
+    addSequential(new C_GoToSelectedLevel());
+  }
+}
